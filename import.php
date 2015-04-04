fbb45b4986223d2ec414ea26f66bb3488453ba30
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-04-04 19:57:33 +0200

    create db; fill db

--- import.php
+++ import.php
@@ -3,9 +3,29 @@
 <?
 $f = fopen("http://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:BAUMOGD&srsName=EPSG:4326&outputFormat=csv&maxFeatures=50", "r");
 $headers = fgetcsv($f);
+$r = array_map("utf8_encode", $headers);
+
+mkdir("data");
+unlink("data/baum.db");
+
+$db = new PDO("sqlite:data/baum.db");
+
+$db->beginTransaction();
+
+$db->query("create table data (". implode(", ", array_map(function($col) {
+  global $db;
+
+  return $db->quote($col) . " text";
+}, $headers)) . ")");
 
 while($r = fgetcsv($f)) {
   $r = array_map("utf8_encode", $r);
-  $data = array_combine($headers, $r);
-  print_r($data);
+
+  $db->query("insert into data values (". implode(", ", array_map(function($v) {
+    global $db;
+
+    return $db->quote($v);
+  }, $r)) . ")");
 }
+
+$db->commit();
