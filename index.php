f4eafca64e588a2961f2b0ba5d81d1afcddad0de
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-04-04 20:08:23 +0200

    Add submodules

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php include "conf.php"; /* load a local configuration */ ?>
 <?php include "modulekit/loader.php"; /* loads all php-includes */ ?>
+<?php call_hooks("init"); ?>
 <html>
   <head>
     <title>Baumkataster Wien</title>
     <?php print modulekit_to_javascript(); /* pass modulekit configuration to JavaScript */ ?>
     <?php print modulekit_include_js(); /* prints all js-includes */ ?>
     <?php print modulekit_include_css(); /* prints all css-includes */ ?>
+    <?php print_add_html_headers(); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
   </body>
