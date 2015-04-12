c4fa18a45ab2ea38b1ff62cdb38556a1dc5c9461
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-04-12 19:08:39 +0200

    DB: define sqlite function 'distance'

--- inc/db.php
+++ inc/db.php
@@ -10,3 +10,22 @@ $db->sqliteCreateFunction('regexp',
         return null;
     }
 );
+
+// from http://stackoverflow.com/a/16032915 (slightly modified)
+// parameters:
+//   lat1, lon1
+//   lat2, lon2
+$db->sqliteCreateFunction('distance',
+  function() {
+    // needs 4 parameters
+    if(count(func_get_args()) != 4)
+      return null;
+
+    // convert to radians
+    $geo = array_map('deg2rad', func_get_args());
+
+    // apply the spherical law of cosines to our latitudes and longitudes, and set the result appropriately
+    // 6378.1 is the approximate radius of the earth in kilometres
+    return acos(sin($geo[0]) * sin($geo[2]) + cos($geo[0]) * cos($geo[2]) * cos($geo[1] - $geo[3])) * 6378140;
+  },
+  4);
