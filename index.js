174f9a03540d8a0e36782e8b4a4ff48f633303b6
Stephan Bösch-Plepelits <skunk@xover.htu.tuwien.ac.at>
2015-04-14 21:08:44 +0200

    Update distances when location changed

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var orig_search_param;
 var data;
 
+function update_distances() {
+  search_param = form_search.get_data();
+
+  for(var i = 0; i < data.data.length; i++) {
+    var el = data.data[i];
+
+    el.distance = haversine({
+      latitude: search_param.location.latitude,
+      longitude: search_param.location.longitude
+    }, {
+      latitude: parseFloat(el.LAT),
+      longitude: parseFloat(el.LON),
+    }, {unit: 'meter'});
+  }
+}
+
 function update_data(search_param, _data) {
   orig_search_param = search_param;
 
@@ -10,6 +26,7 @@ function update_data(search_param, _data) {
   }
 
   data = _data;
+  update_distances();
 
   var content_div = document.getElementById("content");
   var t = new table(table_def, data.data, {
@@ -22,7 +39,10 @@ function update_location() {
   if(!data) {
     search_param = form_search.get_data();
     ajax("data.php", search_param, update_data.bind(this, search_param));
+    return;
   }
+
+  update_distances();
 }
 
 window.onload = function() {
